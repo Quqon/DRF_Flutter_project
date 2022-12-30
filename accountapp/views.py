38a7f21cfb85319654a297673d33d846d979c987
Alan <wjddlsgh0203@gmail.com>
2022-12-30 18:32:54 +0900

    [ADD]backend server

---
+++ accountapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+
+# Create your views here.
+def hello_world(request):
+    return render(request, "accountapp/temp.html");
+
+
+# @api_view()
+# def hello_world(request):
+#     return Response({"message": "hello world!"});
