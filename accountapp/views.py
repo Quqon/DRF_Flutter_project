6e3c2e9c5ad336b8fe3d089e579731926ca0df9e
Alan <wjddlsgh0203@gmail.com>
2022-12-30 22:24:16 +0900

    [MODIFY4]gitignore

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,9 +5,9 @@ from rest_framework.response import Response
 
 # Create your views here.
 def hello_world(request):
-    return render(request, "accountapp/temp.html");
+    return render(request, "accountapp/temp.html")
 
 
 @api_view()
 def hello_world_drf(request):
-    return Response({"message": "hello world!"});
+    return Response({"message": "hello world!"})
